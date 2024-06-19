3023a87d67c0eb85d8f43f698c3bcbf9e0e9656b
Erikaloren <erikalorena43@gmail.com>
2024-06-18 21:47:18 -0500

    Products updated/ images working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 // import {TangerineFontModule} from '@fontsource/tangerine';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavigationComponent],
+  imports: [CommonModule, RouterOutlet, NavigationComponent, NgxPaginationModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
